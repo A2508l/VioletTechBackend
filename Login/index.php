764e3d679834f4263058ead0f2f80ffefa0c087f
anshul <anshulprasad25@gmail.com>
2021-06-07 21:36:17 +0530

    Created and designed Login and signup page

---
+++ Login/index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+     $_SESSION;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <a href="/Login/logout.php">LOGOUT</a>
+    <h1>This is the index page.</h1>
+    <p>Hello, Username</p>
+</body>
+</html>
---
+++ Login/login.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css" type="text/css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="box">
+
+        <form method="post">
+            <div class="heading">Login</div>
+
+            <input id="user_name" type="text" name="user_name" placeholder="Enter username"><br><br>
+            <input id="password" type="password" name="password" placeholder="Enter password"><br><br>
+
+            <input id="button" type="submit" value="Login"><br><br>
+
+            <a href="signup.php">Click to Signup</a><br><br>
+        </form>
+    </div>
+</body>
+
+</html>
